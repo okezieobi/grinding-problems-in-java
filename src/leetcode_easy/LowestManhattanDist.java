719657994f07fc5ed63c6f2977b828cbe03054c7
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-03-12 18:24:29 +0100

    new commit

---
+++ src/leetcode_easy/LowestManhattanDist.java
@@ -0,0 +1,16 @@
+public class LowestManhattanDist {
+   public int nearestValidPoint(int x, int y, int[][] points) {
+	var nearest = -1;
+
+	for (int i = 0, minDistance = Integer.MAX_VALUE; i < points.length; i++)
+		if (x == points[i][0] || y == points[i][1]) { // check valid point
+			var distance = Math.abs(x - points[i][0]) + Math.abs(y - points[i][1]); // find Manhattan distance
+			
+			if (minDistance > distance) { 
+				minDistance = distance; // maintain the nearest valid point
+				nearest = i;
+			}
+		}
+	return nearest;
+}
+}
