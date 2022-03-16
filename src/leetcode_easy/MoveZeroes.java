6092f53faa3d942fac48392db2e18b79f7e030ed
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-03-16 02:11:09 +0100

    done

---
+++ src/leetcode_easy/MoveZeroes.java
@@ -0,0 +1,44 @@
+/*
+Given an integer array nums,
+move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+
+public class MoveZeroes {
+    public void solution(int[] nums) {
+        int[] result = new int[nums.length];
+        int countUp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                result[countUp] = nums[i];
+                countUp++;
+            }
+        }
+        int countDown = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                result[countDown] = nums[i];
+                countDown--;
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            nums[i] = result[i];
+        }
+    }
+}
